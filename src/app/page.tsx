7f044d753b86436b4bd9679e08b2f176f1e11767
Hashir Akbar <hashir1313@gmail.com>
2024-10-13 17:45:00 +0500

    feat: add banana model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { Model } from "./components/models/banana";
+import { Suspense } from "react";
+import { OrbitControls } from "@react-three/drei";
+
 export default function Home() {
-  return <div className="w-full h-full bg-[red]">HELLO</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-96 h-96 bg-gray-100">
+        <Canvas>
+          <Suspense>
+            <ambientLight intensity={1} />
+            <OrbitControls enableZoom />
+            <Model />
+          </Suspense>
+        </Canvas>
+      </div>
+    </div>
+  );
 }
